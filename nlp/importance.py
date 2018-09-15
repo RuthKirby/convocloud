5b8ce80892b56a4868d71840c5c90704b83c0675
Ruth <rb625@kent.ac.uk>
2018-09-16 00:42:38 +0100

    Added method for counting frequency - #1
    Frequency of a word is now reflected in a value preventing duplicates.

--- nlp/importance.py
+++ nlp/importance.py
@@ -15,18 +15,39 @@ def class_rate (words):
     """
     Rates list of word/wordclass tuples
     """
+
     class_rated = []
+    unique = True
+    print(str(words))
     for word, wordclass in words:
-    
-        if wordclass in ('n', 'v'):
 
-            class_rated.append([word, 3])
+        if len(class_rated) > 0:
+            """
+            for seenword, score in class_rated: 
+                if word == seenword:
+                    score = score + 1
+                    print(str(score))
+                    unique = False
+            """
+            for x in class_rated: #Increment score of word if duplicate
+                if x[0] == word:
+                    x[1] = x[1] + 1
+                    unique = False
+
+            #class_rated = [[seenword, score + 1] unique = false if word == seenword else [seenword, score] for seenword, score in class_rated]
+
+        if unique: #Only add to the list of words if it is a new word
+            if wordclass in ('n', 'v'):
+
+                class_rated.append([word, 3])
+            
+            elif wordclass in ('a'):
+                class_rated.append([word, 2])
+
+            else:
+                class_rated.append([word, 1])
         
-        elif wordclass in ('a'):
-            class_rated.append([word, 2])
-
-        else:
-            class_rated.append([word, 1])
+        unique = True
 
     return class_rated
 
